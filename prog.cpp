57ff4acd27be023f9573a0b9c4272d913996b6cb
SHASHWAT KUMAR <500068392@DDN.UPES.AC.IN>
2019-03-31 22:34:48 +0530

    in branch master

---
+++ prog.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class calculator
+{
+	int add(int a,int b)
+	{
+		
+	}
+
+	int sub(int a,int b)
+	{
+
+	}
+
+	int mul(int a,int b)
+	{
+
+	}
+
+	int div(int a,int b)
+	{
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
+
+
+
