844b87b9569e953231f078e747c5b7e522e28f3b
SHASHWAT KUMAR <500068392@DDN.UPES.AC.IN>
2019-03-31 22:37:57 +0530

    in branch branch1

--- prog.cpp
+++ prog.cpp
@@ -5,22 +5,22 @@ class calculator
 {
 	int add(int a,int b)
 	{
-		
+		return a+b;
 	}
 
 	int sub(int a,int b)
 	{
-
+                return a-b;
 	}
 
 	int mul(int a,int b)
 	{
-
+                return a*b;
 	}
 
 	int div(int a,int b)
 	{
-
+                return a/b;
 	}
 };
 
